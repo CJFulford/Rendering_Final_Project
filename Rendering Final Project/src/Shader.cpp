aecdef35f6899e3ffc4541729207db8b7ace707c
CJFulford <cjfulford@shaw.ca>
2016-11-30 17:08:46 -0700

    refactored code in Shaders.cpp to remove code copying. shader comilation code is now much smaller

--- Rendering Final Project/src/Shader.cpp
+++ Rendering Final Project/src/Shader.cpp
@@ -1,226 +1,115 @@
 #include "Shader.h"
 
-Shader::Shader(){}
+Shader::Shader() {}
 Shader::~Shader() {}
 
-unsigned long Shader::getFileLength( std::ifstream& file )
+unsigned long Shader::getFileLength(std::ifstream& file)
 {
-    if (!file.good()) return 0;
+	if (!file.good()) return 0;
 
-    file.seekg(0, std::ios::end);
-    unsigned long len = file.tellg();
-    file.seekg(std::ios::beg);
+	file.seekg(0, std::ios::end);
+	unsigned long len = file.tellg();
+	file.seekg(std::ios::beg);
 
-    return len;
+	return len;
 }
 
-GLchar* Shader::loadshader( std::string filename )
+GLchar* Shader::loadshader(std::string filename)
 {
-    std::ifstream file;
-    file.open(filename.c_str(), std::ios::in); // opens as ASCII!
-    if (!file) return NULL;
+	std::ifstream file;
+	file.open(filename.c_str(), std::ios::in); // opens as ASCII!
+	if (!file) return NULL;
 
-    unsigned long len = getFileLength(file);
+	unsigned long len = getFileLength(file);
 
-    if (len == 0) return NULL; // Error: Empty File
+	if (len == 0) return NULL; // Error: Empty File
 
-    GLchar* ShaderSource = 0;
+	GLchar* ShaderSource = 0;
 
-    ShaderSource = new char[len + 1];
+	ShaderSource = new char[len + 1];
 
-    if (ShaderSource == 0) return NULL; // can't reserve memoryf
+	if (ShaderSource == 0) return NULL; // can't reserve memoryf
 
-    // len isn't always strlen cause some characters are stripped in ascii read...
-    // it is important to 0-terminate the real length later, len is just max possible value...
-    ShaderSource[len] = 0;
+										// len isn't always strlen cause some characters are stripped in ascii read...
+										// it is important to 0-terminate the real length later, len is just max possible value...
+	ShaderSource[len] = 0;
 
-    unsigned int i = 0;
-    while ( file.good() )
-    {
-        ShaderSource[i] = file.get(); // get character from file.
-        if (!file.eof())
-            i++;
-    }
+	unsigned int i = 0;
+	while (file.good())
+	{
+		ShaderSource[i] = file.get(); // get character from file.
+		if (!file.eof())
+			i++;
+	}
 
-    ShaderSource[i] = 0; // 0-terminate it at the correct position
+	ShaderSource[i] = 0; // 0-terminate it at the correct position
 
-    file.close();
+	file.close();
 
-    return ShaderSource; // No Error
+	return ShaderSource; // No Error
 }
 
-void Shader::unloadshader( GLchar** ShaderSource )
+void Shader::unloadshader(GLchar** ShaderSource)
 {
-    if (*ShaderSource != 0)
-        delete[] * ShaderSource;
-    *ShaderSource = 0;
+	if (*ShaderSource != 0)delete[] * ShaderSource;
+	*ShaderSource = 0;
 }
 
 GLuint Shader::compile_shaders(const char* vertexFilename, const char* fragmentFilename)
 {
-	GLuint vertex_shader;
-	GLuint fragment_shader;
-	GLuint program;
+	GLuint program = glCreateProgram();
 
-	const GLchar * vertex_shader_source[] = { loadshader(vertexFilename) };
-	const GLchar * fragment_shader_source[] = { loadshader(fragmentFilename) };
-
-	// Create and compile vertex shader
-	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertex_shader, 1, vertex_shader_source, NULL);
-	glCompileShader(vertex_shader);
-
-	//Create and compile a fragment shader
-	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragment_shader, 1, fragment_shader_source, NULL);
-	glCompileShader(fragment_shader);
-
-
-	// Create program, attach shaders to it, and link it
-	program = glCreateProgram();
-	glAttachShader(program, vertex_shader);
-	glAttachShader(program, fragment_shader);
+	attachShader(program, vertexFilename, GL_VERTEX_SHADER);
+	attachShader(program, fragmentFilename, GL_FRAGMENT_SHADER);
 
 	glLinkProgram(program);
-
-	GLint status;
-
-	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &status);
-	if (status == GL_FALSE)
-	{
-		GLint infoLogLength;
-		glGetShaderiv(vertex_shader, GL_INFO_LOG_LENGTH, &infoLogLength);
-
-		GLchar* strInfoLog = new GLchar[infoLogLength + 1];
-		glGetShaderInfoLog(vertex_shader, infoLogLength, NULL, strInfoLog);
-
-		std::cout << "\n" << vertexFilename << "\t";
-
-		fprintf(stderr, "Compilation error in shader vertex_shader: %s\n", strInfoLog);
-		delete[] strInfoLog;
-
-	}
-
-	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &status);
-	if (status == GL_FALSE)
-	{
-		GLint infoLogLength;
-		glGetShaderiv(fragment_shader, GL_INFO_LOG_LENGTH, &infoLogLength);
-
-		GLchar* strInfoLog = new GLchar[infoLogLength + 1];
-		glGetShaderInfoLog(fragment_shader, infoLogLength, NULL, strInfoLog);
-
-		std::cout << "\n" << fragmentFilename << "\t";
-		fprintf(stderr, "Compilation error in shader fragment_shader: %s\n", strInfoLog);
-		delete[] strInfoLog;
-
-	}
-
-
-
-	// Delete the shaders as the program has them now
-	glDeleteShader(vertex_shader);
-	glDeleteShader(fragment_shader);
-
-	unloadshader((GLchar**)vertex_shader_source);
-	unloadshader((GLchar**)fragment_shader_source);
 
 	return program;
 }
 
 
-GLuint Shader::compile_shaders( const char* vertexFilename, const char* geometryFilename, const char* fragmentFilename)
+GLuint Shader::compile_shaders(const char* vertexFilename, const char* geometryFilename, const char* fragmentFilename)
 {
-    GLuint vertex_shader;
-    GLuint geometry_shader;
-	GLuint fragment_shader;
+	GLuint program = glCreateProgram();
 
-    GLuint program;
+	attachShader(program, vertexFilename, GL_VERTEX_SHADER);
+	attachShader(program, geometryFilename, GL_GEOMETRY_SHADER);
+	attachShader(program, fragmentFilename, GL_FRAGMENT_SHADER);
 
-    const GLchar * vertex_shader_source [] = {loadshader(vertexFilename)};
-    const GLchar * geometry_shader_source [] = {loadshader(geometryFilename)};
-	const GLchar * fragment_shader_source[] = { loadshader(fragmentFilename) };
+	glLinkProgram(program);
 
-    // Create and compile the vertex shader
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, vertex_shader_source, NULL);
-    glCompileShader(vertex_shader);
+	return program;
+}
 
-	//Create and compile the geometry shader
-	geometry_shader = glCreateShader(GL_GEOMETRY_SHADER);
-	glShaderSource(geometry_shader, 1, geometry_shader_source, NULL);
-	glCompileShader(geometry_shader);
+void Shader::attachShader(GLuint &program, const char* fileName, GLuint shaderType)
+{
+	GLuint shader;
+	const GLchar *shaderSource[] = { loadshader(fileName) };
 
-    //Create and compile the fragment shader
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, fragment_shader_source, NULL);
-    glCompileShader(fragment_shader);
+	// Create and compile the vertex shader
+	shader = glCreateShader(shaderType);
+	glShaderSource(shader, 1, shaderSource, NULL);
+	glCompileShader(shader);
+	GLint status;
 
-
-    // Create program, attach shaders to it, and link it
-    program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, geometry_shader);
-    glAttachShader(program, fragment_shader);
-
-    glLinkProgram(program);
-
-    GLint status;
-
-    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &status);
-
-    if (status == GL_FALSE)
-    {
-        GLint infoLogLength;
-        glGetShaderiv(vertex_shader, GL_INFO_LOG_LENGTH, &infoLogLength);
- 
-        GLchar* strInfoLog = new GLchar[infoLogLength + 1];
-        glGetShaderInfoLog(vertex_shader, infoLogLength, NULL, strInfoLog);
-
-		std::cout << "\n" << vertexFilename << "\t";
-		fprintf(stderr, "Compilation error in shader vertex_shader: %s\n", strInfoLog);
-        delete[] strInfoLog;
-    }
-
-    glGetShaderiv(geometry_shader, GL_COMPILE_STATUS, &status);
-
-    if (status == GL_FALSE)
-    {
-        GLint infoLogLength;
-        glGetShaderiv(geometry_shader, GL_INFO_LOG_LENGTH, &infoLogLength);
-
-        GLchar* strInfoLog = new GLchar[infoLogLength + 1];
-        glGetShaderInfoLog(geometry_shader, infoLogLength, NULL, strInfoLog);
-
-		std::cout << "\n" << geometryFilename << "\t";
-        fprintf(stderr, "Compilation error in shader geometry_shader: %s\n", strInfoLog);
-        delete[] strInfoLog;
-    }
-
-	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &status);
-
+	// Check for compilation errors
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
 	if (status == GL_FALSE)
 	{
 		GLint infoLogLength;
-		glGetShaderiv(fragment_shader, GL_INFO_LOG_LENGTH, &infoLogLength);
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
 
 		GLchar* strInfoLog = new GLchar[infoLogLength + 1];
-		glGetShaderInfoLog(fragment_shader, infoLogLength, NULL, strInfoLog);
+		glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
 
-		std::cout << "\n" << fragmentFilename << "\t";
-		fprintf(stderr, "Compilation error in shader fragment_shader: %s\n", strInfoLog);
+		std::cout << "\n" << fileName << "\t";	// Added this here to show where the error originated as "vertex shader" doesnt help when there are 4 of them"
+		fprintf(stderr, "Compilation error in shader vertex_shader: %s\n", strInfoLog);
 		delete[] strInfoLog;
 	}
 
-    // Delete the shaders as the program has them now
-    glDeleteShader(vertex_shader);
-    glDeleteShader(geometry_shader);
-    glDeleteShader(fragment_shader);
 
+	glAttachShader(program, shader);
 
-    unloadshader((GLchar**) vertex_shader_source);
-    unloadshader((GLchar**) geometry_shader_source);
-    unloadshader((GLchar**) fragment_shader_source);
-
-    return program;
+	glDeleteShader(shader);
+	unloadshader((GLchar**)shaderSource);
 }
